96c178f04d961e6043a03a5508a75aa0899b0dfe
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-09-24 22:06:59 +0200

    Added source code

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const resultWrapper = document.querySelector(".result-wrapper");
 
 //Result word and phonetic element
 const result = document.createElement("div");
-//result.id = "result";
-//result.innerHTML = `<div class="result-wrapper">
-//<h3 id="result-word-text"></h3>
-//<h5 id="result-word-phonetic"></h5>
-//<hr />`;
+result.id = "result";
+result.innerHTML = `<div class="result-wrapper">
+<h3 id="result-word-text"></h3>
+<h5 id="result-word-phonetic"></h5>
+<hr />`;
 
 //API for dictionary
 const dictAPI = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
@@ -21,17 +21,53 @@ form.addEventListener("submit", function (e) {
   const inputWord = document.querySelector("#search-word").value;
   //Join search word to dictionary API URL
   const apiURL = `${dictAPI}${inputWord}`;
-  console.log(apiURL);
-  //Get word details
+
   fetch(apiURL)
-    //return response as JSON
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      console.log(data[0].word);
-      console.log(data[0].phonetics);
+      // search word recognized
+
+      // update result wrapper to empty temporarily
+      resultWrapper.innerHTML = ``;
+      // Clone result elements
+      var res = result.cloneNode(true);
+      // display searched word
+      res.querySelector("#result-word-text").innerText = data[0].word;
+
+      // display searched word phonetic
+      if (!!data[0].phonetic) {
+        res.querySelector("#result-word-phonetic").innerText = data[0].phonetic;
+      } else {
+        if (!!data[0].phonetics) {
+          Object.values(data[0].phonetics).map((phonetic) => {
+            if (!!phonetic.text) {
+              res.querySelector("#result-word-phonetic").innerText =
+                phonetic.text;
+              return false;
+            }
+          });
+        }
+      }
+      Object.values(data[0].meanings).map((meaning) => {
+        var meanings = ``;
+        // display searched word part of speech
+        meanings = `<h5 class="partOfSpeech">${meaning.partOfSpeech}</h5>`;
+        Object.values(meaning.definitions).map((definition) => {
+          // display searched word meaning
+          meanings += `<p class="meaning">${definition.definition}</p>`;
+        });
+        res.innerHTML += `<div class="result-item">${meanings}</div>`;
+      });
+      //updating the result wrapper with the actual result
+      resultWrapper.appendChild(res);
+    })
+    .catch((err) => {
+      // show dialog box if entered or searched word in unknown
+      alert("The word entered is Unknown.");
+      console.error(err);
     });
 
   //Clearing form after submit
